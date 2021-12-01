9411cac762223f07ccb2a48cc011e34fa6770142
Jimmy Xu <xjimmyshcn@gmail.com>
2021-12-01 22:39:27 +0800

    update Dockerfile

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo HTTP_PROXY=$http_proxy
+echo HTTP_PROXY=$https_proxy
 docker build --build-arg HTTP_PROXY=$http_proxy --build-arg HTTPS_PROXY=$https_proxy -t xjimmyshcn/fastapi-web-starter:20211130 .
-
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo http_proxy=$http_proxy
+echo https_proxy=$https_proxy
 docker run -d  --name fastapi-web-starter -p 8080:8080 \
   -e http_proxy=$http_proxy -e https_proxy=$https_proxy \
   -v $PWD/.env:/fastapi-web-starter/.env xjimmyshcn/fastapi-web-starter:20211130
